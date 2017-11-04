cc1bbfe981c1452371b9a94393ed6e24ed0c41f5
Vansh Ralhan <ralhanvansh5@gmail.com>
2017-11-05 03:33:03 +0530

    Exc 9

--- exc9.rb
+++ exc9.rb
@@ -1,12 +1,14 @@
 # Here's some new strange stuff, remember type it exactly
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
+#Way 1 to write string in multiple lines using \n
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 # #{} cannot be in single quotes
 puts "Here are the days: #{days}"
 puts "Here are the months: #{months}"
 
+#Second Way to write string in multiple lines
 puts %q{
 There's something going on here.
 With this weird quote
