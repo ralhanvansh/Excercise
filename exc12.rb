a6d27a3b654688b5835008dc6a369183a18d54d4
Vansh Ralhan <ralhanvansh5@gmail.com>
2017-11-05 11:52:36 +0530

    Exc12 Drill

--- exc12.rb
+++ exc12.rb
@@ -10,3 +10,23 @@ number = another.to_i
 
 smaller = number / 100
 puts "A smaller number is #{smaller}."
+
+# Drill 1
+print "Give me a number: "
+number = gets.chomp.to_f
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_f
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+# Drill 2
+puts "Enter the amount you will give."
+money = gets.chomp.to_f
+
+puts "You will get #{money / 10}"
