50506a821396e3d98665640f55a56fba3c502ef8
puneet18190 <dev@173-168-100-40.res.bhn.net>
2017-11-06 14:25:04 +0530

    Exc15 Drill

--- exc15.rb
+++ exc15.rb
@@ -1,3 +1,4 @@
+=begin
 #getting file name from user
 filename1, filename2 = ARGV
 
@@ -12,3 +13,12 @@ puts "Here's your second file #{filename2}"
 txt_again = open(filename2)
 
 print txt_again.read
+=end
+
+
+#using only gets.chomp to get filename from user
+puts "Enter the file you want to read: "
+file1 = gets.chomp
+open1 = open(file1)
+print open1.read
+open1.close		
